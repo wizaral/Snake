004206f65c0bc4f5614c768b2aa4ea287ccc9db2
wizaral <alexafa@ukr.net>
2020-08-14 23:25:41 +0300

    add base body class

---
+++ game/base/inc/body.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <array>
+#include <deque>
+#include "coordinate.hpp"
+#include "direction.hpp"
+
+class Body {
+protected:
+    std::deque<Coordinate> body_;
+    Direction direction_;
+
+public:
+    virtual ~Body() = default;
+
+    template <class It>
+    Body(It first, It last)
+        : body_(first, last)
+        , direction_(head(body_)) {}
+
+    Direction direction() const;
+    const std::deque<Coordinate> &body() const;
+
+    virtual void step();
+    virtual void grow();
+    virtual void turn(Direction direction);
+    virtual void ultimate();
+
+protected:
+    static Direction head(const std::deque<Coordinate> &body);
+    static Direction direction(const Coordinate &c);
+
+    inline static constexpr std::array<Coordinate, 4> vectors{
+        Coordinate{+0, +1},
+        Coordinate{+1, +0},
+        Coordinate{-0, -1},
+        Coordinate{-1, -0},
+    };
+};
---
+++ game/base/src/body.cpp
@@ -0,0 +1,32 @@
+#include "body.hpp"
+
+Direction Body::direction() const {
+    return direction_;
+}
+
+const std::deque<Coordinate> &Body::body() const {
+    return body_;
+}
+
+void Body::step() {
+    grow();
+    body_.pop_back(); // no size check
+}
+
+void Body::grow() {
+    body_.push_front(body_.front() + vectors[static_cast<int>(direction_)]);
+}
+
+void Body::turn(Direction direction) {
+    direction_ = direction;
+}
+
+void Body::ultimate() {}
+
+Direction Body::head(const std::deque<Coordinate> &body) {
+    return direction(body[0] - body[1]);
+}
+
+Direction Body::direction(const Coordinate &c) {
+    return static_cast<Direction>(((c.x + c.y) < 0) * 2 + (c.x != 0));
+}
